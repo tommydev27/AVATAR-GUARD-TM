63bc8be1c2390a669cb1978994e4c46a6b38cfc8
Tommy <tengkorak997@gmail.com>
2026-02-12 06:11:36 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-<script>
-  
+
   
 const injectUI = () => {
     // LOGIKA BARU: Diambil dari assets/js/app-53e5ebda.js
@@ -469,6 +459,3 @@ floatBtn.onclick = () => {
 
 injectUI();
   
-</script>
-</body>
-</html>
